f821817324eac5cce5ffd5da3c731b0d0234fe72
Ghost-script <subho.prp@gmail.com>
2015-07-21 12:19:45 +0530

    fixed settings.py

--- wsgi/taskman/taskman/settings.py
+++ wsgi/taskman/taskman/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    #'debug_toolbar',
     'taskman',
     'taskManager',
 )
@@ -107,6 +107,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
     )
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
